5414617ad4217702647ebb1b5ce7d4d60f6ffa69
Devin Sweet <sweetdevin29@gmail.com>
2024-09-21 22:28:48 -0400

    update to combat and mana check functions, no longer loses turn for failing mana check or value error

--- class test.py
+++ class test.py
@@ -1,4 +1,6 @@
 from random import randint
+# initial living creature class all player and 
+# npc will have this super class as of right now
 class creature:
     def __init__(self, name,) -> None:
         self.name = name
@@ -9,30 +11,39 @@ class creature:
         self.def_val = 10
         self.weapon = 'body'
         self.attacks = [self.basic_attack]
+        self.attacks_str = ['basic attack']
     def __repr__(self):
         return f'name is {self.name} \n health = {self.health} \n mana = {self.mana}'
-
+    #basic attack function
     def basic_attack(self, target):
         damage = (self.atk_val + randint(0, 20)) - target.def_val
         target.health -= damage
         print(f'{self.name} hit {target.name} with {self.weapon} for {damage} damage')
     def speak(self):
         return f"Hi, my name is {self.name}"
-    def mana_check(self, cost):
+    #mana check to be used before all mana costing attacks, 
+    #makes sure self has mana if it does not it performs basic attck
+    def mana_check(self, target, cost):
         if self.mana < cost:
-            print('you lack the mind to perform this ability and falter')
+            print(f'{self.name} lacks the mind to perform this ability and does a basic attack')
+            self.basic_attack(target)
             return False
         return True
     def sound(self):
         return self.noise
+    #combat function engages an npc in combat collect input and calls attacks
+    #repeats until someone's health reaches 0
     def enter_combat(self, target):
-        count = 0
-        for x in self.attacks:
-            print(f'{count} for {x} ')
-        player_input = input('what do you do?')
-        attack = self.attacks[int(player_input)]
+        print(f'choose your action \n {self.attacks_str}')
+        player_input = input('')
+        try:
+           index = self.attacks_str.index(player_input)
+        except ValueError:
+            print(f"in {self.name}'s confusion they use a basic attack")
+            index = 0
+        attack = self.attacks[index]
         attack(target)
-        npc_attack = target.attacks[randint(0, len(target.attacks))]
+        npc_attack = target.attacks[randint(0, len(target.attacks) - 1)]
         npc_attack(self)
         if target.health <= 0: 
             print(f'{self.name} is victorious')
@@ -41,6 +52,7 @@ class creature:
             print(f'{self.name} has died')
             return
         return self.enter_combat(target)
+#murlock subclass
 class murlock(creature):
     def __init__(self, name) -> None:
         super().__init__(name)
@@ -48,15 +60,17 @@ class murlock(creature):
         self.weapon = 'claws'
         self.mana = 25
         self.attacks = [self.basic_attack, self.bubble_atk]
+        self.attacks_str = ['basic attack', 'bubble attack']
     def speak(self):
         return f'blugblug {self.name} blugblug'
+    #murlocks special attack
     def bubble_atk(self, target, cost = 10):
-        if self.mana_check(cost) == True:
-            self.mana -= 10
+        if self.mana_check(target, cost) == True:
+            self.mana -= cost
             damage = randint(0, 20) + 20
             target.health -= damage
             print(f'{self.name} launches bubbles at {target.name} for {damage} damage')
-
+# barbarian subclass
 class barbarian(creature):
     def __init__(self, name,):
         super().__init__(name)
@@ -65,14 +79,17 @@ class barbarian(creature):
         self.weapon = 'club'
         self.mana = 15
         self.attacks = [self.basic_attack, self.leaping_smash]
+        self.attacks_str = ['basic attack', 'leaping attack']
+    #barbarian special attacks
     def leaping_smash(self, target, cost = 5):
-        if self.mana_check(cost) == True:
-            self.mana -= 5
+        if self.mana_check(target, cost) == True:
+            self.mana -= cost
             damage = randint(0, 20) + 35 
             target.health -= damage 
             print(f'{self.name} leaps into the air and smashes his club down on {target.name} for {damage} damage')
 
+#testing code
 dreadclaw = murlock('dreadclaw')
 snagletooth = murlock('snagletooth')
 hulk = barbarian('hulk')
-hulk.enter_combat(dreadclaw)
+dreadclaw.enter_combat(hulk)
