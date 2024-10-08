7b2b01366b6a34e133ad85564d4f3078be259851
Devin Sweet <sweetdevin29@gmail.com>
2024-10-07 22:07:02 -0400

    daily commit
    some big updates for stability and data validation. combat loop update as well is not complete will finish it up tomorrow

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # D_D_style_game
- a personal python project
+ a personal python project 
+ I should rename this as a mud style game, an ode to the many hours of my youth spent online not in chat rooms or hangouts but playing a mud slaying dragons and solving quests.
--- player_class.py
+++ player_class.py
@@ -1,5 +1,4 @@
 from class_test import creature 
-from rooms import roomnode
 from rooms import spawnnode
 
 class player(creature):
@@ -7,7 +6,8 @@ class player(creature):
         super().__init__(name)
         self.location = spawnnode
         self.basic_action = {'look' : self.look, 'travel': self.traverse,
-                             'me': self.me, 'quit': self.quit}
+                             'me': self.me, 'quit': self.quit, 'attack': self.enter_combat}
+        self.health = 500
         self.active = False
     #basic player specific commands
     # a travel function to move the play    
@@ -15,11 +15,12 @@ class player(creature):
         direction_list = [x for x in self.location.exits.keys()]
         print(direction_list)
         direction = input('which way? \n')
-        next_node = self.location.exits[direction]
-        if next_node:
-            self.location = next_node
+        if direction in direction_list:
+            self.location = self.location.exits[direction]
             self.look()
-        else: return 'cannot travel that way'
+            return
+        else: print('cannot travel that way') 
+        return self.traverse()
     # a simple look around or location command
     def look(self):
         print(self.location.description)
@@ -37,7 +38,33 @@ class player(creature):
     def quit(self):
         self.active = False
         print('so long and thanks for all the fish')
-
+    
+    def enter_combat(self):
+        print(self.location.creatures)
+        target = input('attack what?')
+        if target not in self.location.creatures:
+            print('that target does not exist here')
+            return
+        index = self.location.creatures.index(target)
+        self.combat_loop(self.location.creatures[index])
+    def combat_loop(self, target):
+        attacks = [x for x in self.attacks.keys()]
+        print(attacks)
+        player_input = input('what action do you take? \n')
+        if player_input in attacks:
+           self.attacks[player_input](target)
+        else:
+            print(f'{self.name} is confused by your command and uses a basic attack')
+            self.basic_attack(target)
+        npc_attack = randint(0, len(target.attacks.keys()) -1)
+        target.attacks[target.attacks.keys()[npc_attack]](self)
+        if target.health <= 0: 
+            print(f'{self.name} is victorious')
+            return
+        if self.health <= 0:
+            print(f'{self.name} has died')
+            return
+        return self.combat_loop(target)
 
 # a basic play game loop
 def play_game():
@@ -50,11 +77,11 @@ def game_loop(player):
           with no memories of your past, only your name {player.name}''')
     player.look()
     while player.active == True:
-        print('what action do you that?')
+        print('what action do you take?')
         actions = [x for x in player.basic_action.keys()]
         print(actions)
         user_action = input('')
-        if player.basic_action[user_action]:
+        if user_action in actions:
             player.basic_action[user_action]()
         else: print('please select an action')
 
